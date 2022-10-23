ff447cd1d450cd4e7ffb960d463b7f78cfe941be
tina <danatina.dt@gmail.com>
2022-10-23 13:10:43 +0200

    check the expression

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import Login from './Login'
 
 var isLoggedIn = false
 
+const currentHour = new Date().getHours()
+console.log(currentHour)
+
 function App() {
   return (
-    <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
+    <div className="container">
+      {
+        // isLoggedIn ? <h1>Hello</h1> : <Login />
+        currentHour > 12 ? <h1>Why are you still working?</h1> : null
+      }
+    </div>
   )
 }
 
