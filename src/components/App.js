bafebd7358e5f654449847778b6768dacccbc99c
tina <danatina.dt@gmail.com>
2022-10-23 13:01:02 +0200

    delete function renderConditionally

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,6 @@ import Login from './Login'
 
 var isLoggedIn = false
 
-function renderConditionally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>
-  } else {
-    return <Login />
-  }
-}
-
 function App() {
   return (
     <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
