7991b11686a2ce994c287bbff0fde8cd6381e6eb
tina <danatina.dt@gmail.com>
2022-10-23 13:36:40 +0200

    check if registred

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import Login from './Login'
-
-var isLoggedIn = false
-
-const currentHour = new Date().getHours()
-console.log(currentHour)
+import Form from './Form'
+var isUserRegistred = true
 
 function App() {
   return (
     <div className="container">
       {
-        // isLoggedIn ? <h1>Hello</h1> : <Login />
+        <Form isRegistred={isUserRegistred} />
+
+        /* {
+        isLoggedIn ? <h1>Hello</h1> : <Login />
         currentHour > 12 && <h1>Why are you still working?</h1>
+      } */
       }
     </div>
   )
